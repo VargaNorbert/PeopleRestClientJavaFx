140a9edd8e87846c7c3a2f8a1b54ba35a703e4a2
Varga Norbert <vnorbi02@gmail.com>
2022-11-17 08:43:57 +0100

    Cigánynak speciálisan

--- src/main/java/hu/petrik/peoplerestclientjavafx/ListPeopleController.java
+++ src/main/java/hu/petrik/peoplerestclientjavafx/ListPeopleController.java
@@ -1,14 +1,97 @@
 package hu.petrik.peoplerestclientjavafx;
 
+import com.google.gson.Gson;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class ListPeopleController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableView<Person> peopleTable;
+    @FXML
+    private TableColumn<Person,Integer> ageCol;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private Button insertButton;
+    @FXML
+    private TableColumn<Person,String> nameCol;
+    @FXML
+    private TableColumn<Person,String> emailCol;
+    @FXML
+    private Button updateButton;
+
+    @FXML
+    private void initialize(){
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        emailCol.setCellValueFactory(new PropertyValueFactory<>("email"));
+        ageCol.setCellValueFactory(new PropertyValueFactory<>("age"));
+        Platform.runLater(()-> {
+        try {
+            Response response = RequestHandler.get(App.BASE_URL);
+            String content = response.getContent();
+            System.out.println(content);
+            Gson converter = new Gson();
+            Person[] people = converter.fromJson(content,Person[].class);
+            for (Person person: people){
+                peopleTable.getItems().add(person);
+            }
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error!");
+            alert.setHeaderText("Couldn't get data from server");
+            alert.setContentText(e.getMessage());
+
+                alert.showAndWait();
+                Platform.exit();
+            }
+
+        });
+    }
+
+
+    @FXML
+    public void insertClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void deleteClick(ActionEvent actionEvent) {
+        int selectedIndex = peopleTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex == -1){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setHeaderText("Please select a person from the list first");
+            alert.show();
+            return;
+        }
+
+
+        Person selected = peopleTable.getSelectionModel().getSelectedItem();
+        Alert confirmation = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmation.setHeaderText(String.format("Are you sure you want to delete %s",selected.getName()));
+        Optional<ButtonType> optionalButtonType = confirmation.showAndWait();
+        if (optionalButtonType.isEmpty()){
+            System.out.println("Unknown problem");
+            return;
+        }
+        ButtonType clickedButton = optionalButtonType.get();
+        if (clickedButton.equals(ButtonType.OK)){
+            String url = App.BASE_URL + "/" + selected.getId();
+            try {
+                RequestHandler.delete(url);
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+            }
+        }
+
+    }
+
+    @FXML
+    public void updateClick(ActionEvent actionEvent) {
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module hu.petrik.peoplerestclientjavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
-
-    opens hu.petrik.peoplerestclientjavafx to javafx.fxml;
     exports hu.petrik.peoplerestclientjavafx;
+    opens hu.petrik.peoplerestclientjavafx to javafx.fxml,com.google.gson;
 }
